50dd5b304791d04716255eea7e3f78b94804154f
ExGules <Respekt32113113@yandex.ru>
2024-09-18 00:36:40 +0400

    ПР9 Изменения 3.0

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { LarekAPI } from '../src/components/ApiLarek';
 import { API_URL, CDN_URL } from './utils/constants';
 import { EventEmitter } from './components/base/events';
 import {
-	AppState,
-	CatalogChangeEvent
+  AppState,
+  CatalogChangeEvent
 } from './components/AppData';
 import { Page } from './components/Page';
 import { Modal } from './components/common/Modal';
@@ -38,197 +38,201 @@ const order = new Order(cloneTemplate(orderTemplate), events);
 const contacts = new Contacts(cloneTemplate(contactsTemplate), events);
 
 events.onAll(({ eventName, data }) => {
-	console.log(eventName, data);
+  console.log(eventName, data);
 });
 
 events.on<CatalogChangeEvent>('items:changed', () => {
-	const cards = appData.catalog.map((item) => {
-		const card = new CatalogItem(cloneTemplate(cardCatalogTemplate), {
-			onClick: () => events.emit('card:select', item),
-		});
-		return card.render({
-			title: item.title,
-			image: item.image,
-			description: item.description,
-			price: item.price,
-			category: item.category,
-			id: item.id,
-		});
-	});
-	
-	page.catalog = cards;
+  const cards = appData.catalog.map((item) => {
+    const card = new CatalogItem(cloneTemplate(cardCatalogTemplate), {
+      onClick: () => events.emit('card:select', item),
+    });
+    return card.render({
+      title: item.title,
+      image: item.image,
+      description: item.description,
+      price: item.price,
+      category: item.category,
+      id: item.id,
+    });
+  });
+
+  page.catalog = cards;
 });
 
 events.on('card:select', (item: IProduct) => {
-	appData.setPreview(item);
+  appData.setPreview(item);
 });
 
 events.on('preview:changed', (item: IProduct) => {
-	const card = new CatalogItem(cloneTemplate(cardPreviewTemplate));
-	
-	card.addToCart = () => {
-		appData.toggleOrderedItem(item.id, true);
-		card.buttonText = 'Удалить'; 
-		modal.close(); 
-		events.emit('larek:changed');
-		basket.selected = appData.getTotal();
-	};
+  const card = new CatalogItem(cloneTemplate(cardPreviewTemplate));
 
-	if (appData.order.items.includes(item.id)) {
-		card.buttonText = 'Удалить'; 
-		card.addToCart = () => {
-			appData.toggleOrderedItem(item.id, false);
-			card.buttonText = 'В корзину'; 
-			modal.close(); 
-			events.emit('larek:changed');
-			basket.selected = appData.getTotal();
-		};
-	}
-	modal.render({
-		content: card.render({
-			title: item.title,
-			image: item.image,
-			description: item.description,
-			category: item.category,
-			price: item.price,
-			id: item.id,
-		}),
-	});
+  if (appData.order.items.includes(item.id)) {
+    card.buttonText = 'Удалить';
+    card.addToCart = () => {
+      events.emit('product:remove', { id: item.id });
+    };
+  } else {
+    card.addToCart = () => {
+      events.emit('product:add', { id: item.id });
+    };
+  }
+  modal.render({
+    content: card.render({
+      title: item.title,
+      image: item.image,
+      description: item.description,
+      category: item.category,
+      price: item.price,
+      id: item.id,
+    }),
+  });
+});
+
+function updateBasketSelected() {
+  basket.selected = appData.getTotal();
+}
+
+events.on('product:add', ({ id }: { id: string }) => {
+  appData.toggleOrderedItem(id, true);
+  events.emit('larek:changed');
+  updateBasketSelected();
+});
+
+events.on('product:remove', ({ id }: { id: string }) => {
+  appData.toggleOrderedItem(id, false);
+  events.emit('larek:changed');
+  updateBasketSelected();
 });
 
 events.on('larek:changed', () => {
-	page.counter = appData.itemCount();
-	basket.total = appData.getTotal();
-	basket.items = appData.getSelectedItems().map((item, index) => {
-		const card = new BasketItem(cloneTemplate(cardBasketTemplate), {
-			onClick: () => {
-				appData.toggleOrderedItem(item.id, false);
-				page.counter = appData.itemCount();
-				basket.total = appData.getTotal();
-				basket.selected = appData.getTotal();
-				events.emit('larek:changed');
-			},
-		});
-		
-		card.setIndex(index);
-		return card.render({
-			title: item.title,
-			price: item.price,
-		});
-	});
+  page.counter = appData.itemCount();
+  basket.total = appData.getTotal();
+  basket.items = appData.getSelectedItems().map((item, index) => {
+    const card = new BasketItem(cloneTemplate(cardBasketTemplate), {
+      onClick: () => {
+        events.emit('product:remove', { id: item.id });
+      },
+    });
+
+    card.setIndex(index);
+    return card.render({
+      title: item.title,
+      price: item.price,
+    });
+  });
 });
 
 events.on('basket:open', () => {
-	modal.render({
-		content: basket.render(),
-	});
+  modal.render({
+    content: basket.render(),
+  });
 });
 
 events.on('order:open', () => {
-	modal.render({
-		content: order.render({
-			payment: 'online',
-			address: '',
-			valid: false,
-			errors: [],
-		}),
-	});
+  modal.render({
+    content: order.render({
+      payment: 'online',
+      address: '',
+      valid: false,
+      errors: [],
+    }),
+  });
 });
 
 events.on(
-	/^order\..*:change/,
-	(data: { field: keyof IOrderForm; value: string }) => {
-		appData.setOrderField(data.field, data.value);
-	}
+  /^order\..*:change/,
+  (data: { field: keyof IOrderForm; value: string }) => {
+    appData.setOrderField(data.field, data.value);
+  }
 );
 
 events.on(
-	'payment:change',
-	(data: {
-		payment: 'cash' | 'online';
-		clickedButton: HTMLButtonElement;
-		otherButton: HTMLButtonElement;
-	}) => {
-		order.toggleActiveButton(data.clickedButton, data.otherButton);
-		appData.setPaymentField(data.payment);
-		appData.validateOrder();
-	}
+  'payment:change',
+  (data: {
+    payment: 'cash' | 'online';
+    clickedButton: HTMLButtonElement;
+    otherButton: HTMLButtonElement;
+  }) => {
+    order.toggleActiveButton(data.clickedButton, data.otherButton);
+    appData.setPaymentField(data.payment);
+    appData.validateOrder();
+  }
 );
 
+function updateFormErrors(errors: Partial<IOrderForm>, form: Order | Contacts) {
+  const { payment, address } = errors;
+  form.valid = !payment && !address;
+  form.errors = Object.values({ payment, address })
+    .filter((i) => !!i)
+    .join('; ');
+}
+
 events.on('formErrors:change', (errors: Partial<IOrderForm>) => {
-	const { payment, address } = errors;
-	order.valid = !payment && !address;
-	order.errors = Object.values({ payment, address })
-		.filter((i) => !!i)
-		.join('; ');
+  updateFormErrors(errors, order);
 });
 
 events.on('contacts:open', () => {
-	modal.render({
-		content: contacts.render({
-			phone: '',
-			email: '',
-			valid: false,
-			errors: [],
-		}),
-	});
+  modal.render({
+    content: contacts.render({
+      phone: '',
+      email: '',
+      valid: false,
+      errors: [],
+    }),
+  });
+});
+
+events.on('formErrors:contactsChange', (errors: Partial<IOrderForm>) => {
+  updateFormErrors(errors, contacts);
 });
 
 events.on(
-	/^contacts\..*:change/,
-	(data: { field: keyof IOrderForm; value: string }) => {
-		appData.setContactField(data.field, data.value);
-	}
+  /^contacts\..*:change/,
+  (data: { field: keyof IOrderForm; value: string }) => {
+    appData.setContactField(data.field, data.value);
+  }
 );
 
-events.on('formErrors:contactsChange', (errors: Partial<IOrderForm>) => {
-	const { email, phone } = errors;
-	contacts.valid = !email && !phone;
-	contacts.errors = Object.values({ phone, email })
-		.filter((i) => !!i)
-		.join('; ');
-});
-
 events.on('contacts:submit', () => {
-	api
-		.orderProduct(appData.order)
-		.then((result) => {
-			const totalDescription = result.total.toString();
-			const success = new Success(
-				cloneTemplate(successTemplate),
-				totalDescription,
-				{
-					onClick: () => {
-						modal.close();
-						events.emit('larek:changed');
-						basket.selected = appData.getTotal();
-					},
-				}
-			);
-			appData.clearBasket();
-			events.emit('larek:changed');
-			basket.selected = appData.getTotal();
-			modal.render({
-				content: success.render({}),
-			});
-		})
-		.catch((err) => {
-			console.error(err);
-		});
+  api
+    .orderProduct(appData.order)
+    .then((result) => {
+      const totalDescription = result.total.toString();
+      const success = new Success(
+        cloneTemplate(successTemplate),
+        totalDescription,
+        {
+          onClick: () => {
+            modal.close();
+            events.emit('larek:changed');
+            updateBasketSelected();
+          },
+        }
+      );
+      appData.clearBasket();
+      events.emit('larek:changed');
+      updateBasketSelected();
+      modal.render({
+        content: success.render({}),
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
 events.on('modal:open', () => {
-	page.locked = true;
+  page.locked = true;
 });
 
 events.on('modal:close', () => {
-	page.locked = false;
+  page.locked = false;
 });
 
 api.getProductList()
-	.then((items) => {
-		appData.setCatalog(items);
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+  .then((items) => {
+    appData.setCatalog(items);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
